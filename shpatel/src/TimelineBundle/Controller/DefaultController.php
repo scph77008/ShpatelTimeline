a1faf35632f7009f615225a508a9050d74185f8e
Андрей <andrey@openstart.ru>
2016-07-14 18:47:52 +0300

    Тестовый вывод котиков <3

--- shpatel/src/TimelineBundle/Controller/DefaultController.php
+++ shpatel/src/TimelineBundle/Controller/DefaultController.php
@@ -3,11 +3,22 @@
 namespace TimelineBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use TimelineBundle\Entity\Cat;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('TimelineBundle:Default:index.html.twig');
+	    /** @type Cat[] $cats */
+	    $cats = $this->getDoctrine()->getRepository('TimelineBundle:Cat')->findAll();
+	
+	    $choices = [];
+	    foreach ($cats as &$cat)
+	    {
+		   $choices[] = ['id' => $cat->getId(), 'name' => $cat->getName()];
+	    }
+	    unset($cat);
+	   
+        return $this->render('TimelineBundle:Default:index.html.twig', ['cats' => $choices]);
     }
 }
