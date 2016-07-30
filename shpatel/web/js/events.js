e5ccb0a5fe3199a88b993adef2e173671b4121c9
Андрей-W81 <scph77008@gmail.com>
2016-07-30 15:09:18 +0300

    Нашёл место, где выводятся подписи к точкам графика

--- shpatel/web/js/events.js
+++ shpatel/web/js/events.js
@@ -3,14 +3,27 @@ $(function () {
 
     $.getJSON('/app_dev.php/api/?entity=Event', function (data) {
         var seriesData = [];
+        var dataLabels = [];
 
         data.map(function (event) {
             seriesData.push([
                 event.time.timestamp * JS_TIME_MODIFIER, (event.weight > 0 ? event.weight : null)
             ]);
+
+            // TODO: понять, что сюда кидать
+            dataLabels.push(event.catId);
+
         });
         $('#charts').highcharts('StockChart',
             {
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        }
+                    }
+                },
+
                 title: {
                     text: 'CATS! '
                 },
@@ -25,6 +38,8 @@ $(function () {
                         name: 'shpatelek',
                         data: seriesData,
                         connectNulls: true // Соеднияем точки без веса по последнему значению
+                        // TODO: каринки в datalabels
+                        // dataLabels: dataLabels
                     }
                 ]
 
