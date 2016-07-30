7b7869ae8ef32fdb503e04a22065c34668ff89e6
Андрей-W81 <scph77008@gmail.com>
2016-07-30 13:17:29 +0300

    Фикс дат в JS

--- shpatel/web/js/events.js
+++ shpatel/web/js/events.js
@@ -1,31 +1,33 @@
-$(function ()
-{
-	$.getJSON('/app_dev.php/api/?entity=Event', function (data)
-	{
-		var seriesData = [];
+$(function () {
+    const JS_TIME_MODIFIER = 1000;
 
-		data.map( function(event) {
-			seriesData .push([
-				event.time.timestamp, event.weight
-			]);
-		});
-		var series = [{name:'shpatelek', data: seriesData}];
+    $.getJSON('/app_dev.php/api/?entity=Event', function (data) {
+        console.log(data);
+        var seriesData = [];
 
-		console.log(series);
-		$('#charts').highcharts('StockChart',
-		{
-			rangeSelector : {
-				selected : 1
-			},
+        data.map(function (event) {
+            seriesData.push([
+                event.time.timestamp * JS_TIME_MODIFIER, event.weight
+            ]);
 
-			title : {
-				text : 'CATS! '
-			},
+        });
+        var series = [{name: 'shpatelek', data: seriesData}];
+        console.log(series);
+        $('#charts').highcharts('StockChart',
+            {
+                title: {
+                    text: 'CATS! '
+                },
 
-			series : series
-		});
+                xAxis: {
+                    type: 'datetime'
+
+                },
+
+                series: series
+            });
 
 
-	});
-	
+    });
+
 });
