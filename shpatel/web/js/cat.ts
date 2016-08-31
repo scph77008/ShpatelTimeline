27e5abadf15ec8986418d857843e76d1aae1defc
Андрей <andrey@openstart.ru>
2016-08-31 18:51:18 +0300

    TypeScript!

---
+++ shpatel/web/js/cat.ts
@@ -0,0 +1,27 @@
+/**
+ * Кот, пришедший из API
+ * Created by andrey on 31.08.16.
+ */
+function Cat(cat)
+{
+	/**
+	 * @type {number}
+	 */
+	this.id = cat.id;
+
+	/**
+	 * @type {string}
+	 */
+	this.name = cat.name;
+
+	/**
+	 * @type {string}
+	 */
+	this.photo = cat.photo;
+
+	/**
+	 * @type {Date}
+	 */
+	this.datebirth = cat.datebirth;
+
+}
---
+++ shpatel/web/js/event.ts
@@ -0,0 +1,36 @@
+/**
+ * Cобытие, пришедшее из API
+ * Created by andrey on 31.08.16.
+ */
+function Event(event)
+{
+	/**
+	 * @type {number}
+	 */
+	this.id = event.id;
+
+	/**
+	 * @type {number}
+	 */
+	this.catId = event.catId;
+
+	/**
+	 * @type {Date}
+	 */
+	this.time = event.time;
+
+	/**
+	 * @type {string}
+	 */
+	this.photo = event.photo;
+
+	/**
+	 * @type {string}
+	 */
+	this.description = event.description;
+
+	/**
+	 * @type {number}
+	 */
+	this.weight = event.weight;
+}
